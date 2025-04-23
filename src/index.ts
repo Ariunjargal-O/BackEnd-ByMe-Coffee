d268fb9f77b114752863e2d77fe96b078fb58101
24LP2179 24LP2179 <24LP2179@G303-CC0147.local>
2025-04-23 20:28:08 +0800

    first commit

---
+++ src/index.ts
@@ -0,0 +1,14 @@
+import express from "express";
+
+const app = express();
+const port = 8000;
+
+app.get("/", (req, res) => {
+  res
+    .status(200)
+    .json({ success: true, message: "Welcome to ByMe Coffee API" });
+});
+
+app.listen(port, () => {
+  return console.log(`Express is listening at http://localhost:${port}`);
+});
